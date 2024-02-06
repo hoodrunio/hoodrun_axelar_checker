8eab316c8eece1b59661982e3d2224d90e7d24d0
amorf <fatihermetin@gmail.com>
2024-02-06 18:20:21 -0500

    Add chat saver middleware

---
+++ src/bot/tg/middlewares/chatSaverMiddleware.ts
@@ -0,0 +1,17 @@
+import { AppDb } from "@database/database";
+import { TelegramChatType } from "@database/models/telegram_user/telegram_user.interface";
+import { Context, NextFunction } from "grammy";
+
+export async function chatSaver(
+  ctx: Context,
+  next: NextFunction,
+  db: AppDb
+): Promise<void> {
+  db.telegramUserRepo.upsertOne({
+    chat_id: ctx.chat?.id,
+    type: ctx.chat?.type as TelegramChatType,
+    _chat: ctx.chat,
+  });
+
+  await next();
+}
