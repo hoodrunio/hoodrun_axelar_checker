097172dbbf7980468cc1df1236c0639b50adc793
amorf <fatihermetin@gmail.com>
2024-02-06 00:41:08 -0500

    Add validator model props

--- src/database/models/validator/validator.interface.ts
+++ src/database/models/validator/validator.interface.ts
@@ -1,8 +1,23 @@
+import { IBaseInterface, IBaselModel } from "@database/base/model.interface";
+import {
+  Commission,
+  ConsensusPubkey,
+  Description,
+} from "@services/rest/interfaces/validators/validator";
 import { Document } from "mongoose";
-import { IBaseInterface, IBaselModel } from "src/database/base/model.interface";
 
 export interface IValidator extends IBaseInterface {
-  operatorAddress: string;
+  operator_address: string;
+  consensus_pubkey: ConsensusPubkey;
+  jailed: boolean;
+  status: string;
+  tokens: string;
+  delegator_shares: string;
+  description: Description;
+  unbonding_height: string;
+  unbonding_time: string;
+  commission: Commission;
+  min_self_delegation: string;
 }
 
 export interface IValidatorDocument extends Document, IValidator {}
--- src/database/models/validator/validator.model.ts
+++ src/database/models/validator/validator.model.ts
@@ -1,12 +1,38 @@
 import { model, Schema } from "mongoose";
 import { IValidatorDocument } from "./validator.interface";
-import { createAppSchema } from "src/database/helper";
+import { createAppSchema } from "@database/helper";
 
 const VALIDATOR_COLLECTION_NAME = "validators";
 
 const ValidatorSchema: Schema<IValidatorDocument> =
   createAppSchema<IValidatorDocument>({
-    operatorAddress: { type: String, required: true, unique: true },
+    operator_address: { type: String, required: true, unique: true },
+    consensus_pubkey: {
+      "@type": { type: String, required: true },
+      key: { type: String, required: true },
+    },
+    jailed: { type: Boolean, required: true },
+    status: { type: String, required: true },
+    tokens: { type: String, required: true },
+    delegator_shares: { type: String, required: true },
+    description: {
+      moniker: { type: String },
+      identity: { type: String },
+      website: { type: String },
+      security_contact: { type: String },
+      details: { type: String },
+    },
+    unbonding_height: { type: String, required: true },
+    unbonding_time: { type: String, required: true },
+    commission: {
+      commission_rates: {
+        rate: { type: String, required: true },
+        max_rate: { type: String, required: true },
+        max_change_rate: { type: String, required: true },
+      },
+      update_time: { type: String, required: true },
+    },
+    min_self_delegation: { type: String, required: true },
   });
 
 ValidatorSchema.statics.buildModel = (args: IValidatorDocument) => {
