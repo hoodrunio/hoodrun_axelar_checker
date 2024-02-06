e7bcc873cc7dc75ea5dddc3818b674cf0db7134c
amorf <fatihermetin@gmail.com>
2024-02-06 18:36:31 -0500

    Add successful operator addres tgReply method

--- src/bot/tg/TGReply.ts
+++ src/bot/tg/TGReply.ts
@@ -2,4 +2,8 @@ export class TgReply {
   startReply() {
     return `<b style="text-align:center"><strong>Welcome HoodRun Axelar Validator Checker</strong></b>`;
   }
+
+  successFullAddOperatorAddress(operatorAddress: string) {
+    return `Operator address ${operatorAddress} has been added to the chat`;
+  }
 }
