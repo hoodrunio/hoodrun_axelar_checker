975d52a1ed71a93f417f2a809727a8d5ef4d8cbb
amorf <fatihermetin@gmail.com>
2024-02-07 14:33:05 -0500

    Add update validator supported Chains on db method to app class

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,6 +2,8 @@ import { connectDb } from "@database/index";
 import { AxelarQueryService } from "../services/rest/AxelarQueryService";
 import { Validator } from "../services/rest/interfaces/validators/validator";
 import { TGBot } from "bot/tg/TGBot";
+import { logger } from "@utils/logger";
+import { AppDb } from "@database/database";
 
 class App {
   axelarQueryService: AxelarQueryService;
@@ -34,6 +36,79 @@ class App {
       throw error;
     }
   }
+
+  async updateValidatorSupportedChains() {
+    const db = new AppDb();
+    const validators = await db.validatorRepository.findAll();
+    const chainsMaintainers =
+      await this.axelarQueryService.getAxelarAllEvmChainsWithMaintainers();
+
+    const promises = validators.map(async (validator) => {
+      const valSupportedChains: string[] = [];
+      const operatorAddress = validator.operator_address;
+
+      for (const [chain, maintainers] of chainsMaintainers.entries()) {
+        if (maintainers.includes(operatorAddress)) {
+          valSupportedChains.push(chain);
+        }
+      }
+
+      try {
+        await db.validatorRepository.updateValidatorSupportedEvmChains(
+          operatorAddress,
+          valSupportedChains
+        );
+      } catch (error) {
+        logger.error(
+          `Something went wrong while updating validator supported evm chains. Error: ${error}`
+        );
+      }
+    });
+
+    try {
+      await Promise.all(promises);
+    } catch (error) {
+      logger.error(
+        `Something went wrong while running concurrent supported evm chains Promisses. Error: ${error}`
+      );
+    }
+  }
 }
+
+// async function updateValidatorSupportedChains() {
+//   const validators = await this.database.findValidators({ is_active: true });
+//   const supportedChains = await this.getEvmSupportedChains();
+//   const chainsMaintainers: Record<string, string[]> = {};
+
+//   for (const supportedChain of supportedChains) {
+//     try {
+//       const maintainers = await this.getEvmChainMaintainers(supportedChain);
+//       chainsMaintainers[supportedChain] = maintainers;
+//     } catch (error) {
+//       console.error("Could not fetch supported chain maintainers");
+//       continue;
+//     }
+//   }
+
+//   for (const validator of validators) {
+//     const valSupportedChains: string[] = [];
+//     const operatorAddress = validator.operator_address;
+
+//     for (const [chain, maintainers] of Object.entries(chainsMaintainers)) {
+//       if (maintainers.includes(operatorAddress)) {
+//         valSupportedChains.push(chain);
+//       }
+//     }
+
+//     try {
+//       await this.database.updateValidatorSupportedChains(
+//         operatorAddress,
+//         valSupportedChains
+//       );
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// }
 
 export default App;
