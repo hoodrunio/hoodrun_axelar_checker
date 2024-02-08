a4e968ed086339ab2d9a813424ff4c16b3237275
amorf <fatihermetin@gmail.com>
2024-02-08 18:12:29 -0500

    Fix app initialization

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import App from "./app/App";
 import { logger } from "./utils/logger";
 
+import "@extensions/array.extensions";
+
 logger.info("Starting bot...");
 
 async function main() {
-  const app = new App();
-  await app.initalizeApplication();
+  try {
+    const app = new App();
+    await app.initalizeApplication();
+  } catch (error) {
+    logger.error(error);
+  }
 
   logger.info("Application initialized");
 }
