f46ce8946bd45b78f1c55ec131b1c6d27235c868
amorf <fatihermetin@gmail.com>
2024-02-10 02:37:23 -0500

    Add axelar ws client helper

---
+++ src/ws/client/helper.ts
@@ -0,0 +1,39 @@
+import { MessageEvent } from "ws";
+import { IWsEventData } from "ws/interface/IWsEventMessage";
+
+const parseWsMessageEvent = (event: MessageEvent): string => {
+  let data: string = "Unknown data type received from ws";
+
+  switch (true) {
+    case typeof event.data === "string":
+      data = event.data;
+      break;
+    case event.data instanceof Buffer:
+      data = event.data.toString();
+      break;
+    case event.data instanceof ArrayBuffer:
+      data = new TextDecoder().decode(event.data);
+      break;
+    case Array.isArray(event.data) &&
+      event.data.every((item) => item instanceof Buffer):
+      data = event.data.map((buffer) => buffer.toString()).join("");
+      break;
+    default:
+      console.error("Unexpected data type:", typeof event.data);
+      return data;
+  }
+
+  return data;
+};
+
+export const parseAxelarEventMessage = <T>(
+  event: MessageEvent
+): IWsEventData<T> | undefined => {
+  try {
+    const data = parseWsMessageEvent(event);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error parsing ws message:", error);
+    return undefined;
+  }
+};
