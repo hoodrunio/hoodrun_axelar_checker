7c026e8874f9e2f8f1f27f26621ee218492383fe
amorf <fatihermetin@gmail.com>
2024-02-10 13:57:53 -0500

    Initialize poll repo on app db constructor

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,5 @@
 import { NotificationRepository } from "@repositories/notification/NotificationRepository";
+import { PollRepository } from "@repositories/poll/PollRepository";
 import { TelegramUserRepository } from "@repositories/telegram_user/TelegramUserRepository";
 import { ValidatorRepository } from "@repositories/validator/ValidatorRepository";
 
@@ -6,9 +7,11 @@ export class AppDb {
   validatorRepository: ValidatorRepository;
   telegramUserRepo: TelegramUserRepository;
   notificationRepo: NotificationRepository;
+  pollRepo: PollRepository;
   constructor() {
     this.validatorRepository = new ValidatorRepository();
     this.telegramUserRepo = new TelegramUserRepository();
     this.notificationRepo = new NotificationRepository();
+    this.pollRepo = new PollRepository();
   }
 }
