4ee8b1df1c82b2f916301dc226322ae51f472dcc
amorf <fatihermetin@gmail.com>
2024-02-10 02:36:05 -0500

    Add axelar poll event constant

---
+++ src/ws/event/eventHelper.ts
@@ -0,0 +1,31 @@
+import { IncludedPollEvents } from "./PollSendEvent";
+
+export enum PollEvent {
+  ConfirmDeposit = "ConfirmDeposit",
+  ConfirmDepositStarted = "ConfirmDepositStarted",
+  ConfirmERC20Deposit = "ConfirmERC20Deposit",
+  ConfirmTransferKey = "ConfirmTransferKey",
+  ConfirmKeyTransferStarted = "ConfirmKeyTransferStarted",
+  ConfirmGatewayTx = "ConfirmGatewayTx",
+  ConfirmGatewayTxStarted = "ConfirmGatewayTxStarted",
+  Voted = "Voted",
+}
+export const getStartedPollEvent = (event: IncludedPollEvents) => {
+  switch (event) {
+    case PollEvent.ConfirmDeposit:
+      return PollEvent.ConfirmDepositStarted;
+    case PollEvent.ConfirmERC20Deposit:
+      return PollEvent.ConfirmDepositStarted;
+    case PollEvent.ConfirmTransferKey:
+      return PollEvent.ConfirmKeyTransferStarted;
+    case PollEvent.ConfirmGatewayTx:
+      return PollEvent.ConfirmGatewayTxStarted;
+  }
+};
+export const createPollWsEventQuery = (
+  action: PollEvent,
+  participantEvent: PollEvent
+) =>
+  `tm.event='Tx' AND message.action='${action}' AND axelar.evm.v1beta1.${participantEvent}.participants CONTAINS 'participants'`;
+export const createPollVoteWsEventQuery = (action: PollEvent) =>
+  `tm.event='Tx' AND axelar.vote.v1beta1.${action}.action CONTAINS 'vote'`;
