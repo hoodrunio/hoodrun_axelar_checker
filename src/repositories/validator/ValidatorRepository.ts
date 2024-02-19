e8af07f29818c96f58ec4dc2a17edc1f9f059aff
amorf <fatihermetin@gmail.com>
2024-02-18 23:34:24 -0500

    Add nested rpc array object crud methods for validator model

--- src/repositories/validator/ValidatorRepository.ts
+++ src/repositories/validator/ValidatorRepository.ts
@@ -1,9 +1,11 @@
 import {
   IValidator,
   IValidatorDocument,
+  ValRpcHealthEndpoint,
 } from "@database/models/validator/validator.interface";
 import ValidatorDbModel from "@database/models/validator/validator.model";
 import BaseRepository from "@repositories/base.repository";
+import { FilterQuery } from "mongoose";
 
 export class ValidatorRepository extends BaseRepository<
   IValidator,
@@ -36,5 +38,66 @@ export class ValidatorRepository extends BaseRepository<
       }
     );
     return updatedValidator as IValidatorDocument;
+  }
+
+  async upsertRpcHealthEndpoint(
+    query: FilterQuery<IValidatorDocument>,
+    rpcHealthEndpoint: ValRpcHealthEndpoint
+  ): Promise<IValidatorDocument | null> {
+    const validator = await this.findOne(query);
+    if (!validator) return null;
+
+    // Ensure rpcHealthEndpoints array exists
+    validator.rpc_health_endpoints = validator.rpc_health_endpoints || [];
+
+    // Check if the rpcHealthEndpoint already exists in the array
+    const existingEndpointIndex = validator.rpc_health_endpoints.findIndex(
+      (endpoint) => endpoint.name == rpcHealthEndpoint.name
+    );
+
+    // If the endpoint already exists, update it
+    if (existingEndpointIndex !== -1) {
+      const existingEndpoint =
+        validator.rpc_health_endpoints[existingEndpointIndex];
+      validator.rpc_health_endpoints[existingEndpointIndex] = rpcHealthEndpoint;
+    } else {
+      // Otherwise, push the new rpcHealthEndpoint into the array
+      validator.rpc_health_endpoints.push(rpcHealthEndpoint);
+    }
+
+    await validator.save();
+    return validator;
+  }
+
+  async removeRpcHealthEndpoint(
+    query: FilterQuery<IValidatorDocument>,
+    rpcHealthEndpointName: string
+  ): Promise<IValidatorDocument | null> {
+    const validator = await this.findOne(query);
+    if (!validator) return null;
+
+    validator.rpc_health_endpoints = validator?.rpc_health_endpoints ?? [];
+
+    // Remove the rpcHealthEndpoint with the specified name
+    validator.rpc_health_endpoints = validator.rpc_health_endpoints.filter(
+      (endpoint) => endpoint.name !== rpcHealthEndpointName
+    );
+    await validator.save();
+    return validator;
+  }
+
+  async getRpcHealthEndpointWithName(
+    query: FilterQuery<IValidatorDocument>,
+    rpcHealthEndpointName: string
+  ): Promise<ValRpcHealthEndpoint | null> {
+    const validator = await this.findOne(query);
+    if (!validator || !validator.rpc_health_endpoints) return null;
+
+    // Find the rpcHealthEndpoint by name
+    const endpoint = validator.rpc_health_endpoints.find(
+      (endpoint) => endpoint.name === rpcHealthEndpointName
+    );
+
+    return endpoint || null;
   }
 }
